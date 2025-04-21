21fecc1bbf4c7bb0c1d626d19c4df8207aa32bfd
cazdemun <cplu1509@gmail.com>
2025-04-21 12:38:57 -0500

    chore: move app to single file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,7 @@
-import Image from "next/image";
-import Section from "@/app/UI/Section";
-
-import styles from "./page.module.scss";
+import App from "@/app/screens/App";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <header className={styles.header} />
-      <main className={styles.main}>
-        <Section className={styles.textAlignCenter}>
-          <h1>¡Muy pronto podrás construir tu propia página web aquí en Yuntayki!</h1>
-          <p>Estamos preparando una herramienta increíble para que puedas lanzar tu sitio web de manera fácil, rápida y profesional.</p>
-        </Section>
-        <Section className={styles.textAlignCenter}>
-          Holi
-        </Section>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://wa.me/51933081833?text=Hola%2C%20quiero%20crear%20mi%20página%20web%20con%20Yuntayki"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.textAlignCenter}
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          ¿No puedes esperar? ¡Contáctanos! →
-        </a>
-      </footer>
-    </div>
+    <App />
   );
 }
---
+++ src/app/screens/App.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Section from "@/app/UI/Section";
+
+import styles from "./App.module.scss";
+
+export default function App() {
+  return (
+    <div className={styles.page}>
+      <header className={styles.header} />
+      <main className={styles.main}>
+        <Section className={styles.textAlignCenter}>
+          <h1>¡Muy pronto podrás construir tu propia página web aquí en Yuntayki!</h1>
+          <p>Estamos preparando una herramienta increíble para que puedas lanzar tu sitio web de manera fácil, rápida y profesional.</p>
+        </Section>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://wa.me/51933081833?text=Hola%2C%20quiero%20crear%20mi%20página%20web%20con%20Yuntayki"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.textAlignCenter}
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          ¿No puedes esperar? ¡Contáctanos! →
+        </a>
+      </footer>
+    </div>
+  );
+}
