03e3b031b884ec5be7047d66a508c04ed4809266
cazdemun <cplu1509@gmail.com>
2025-04-22 17:26:40 -0500

    chore: install shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.scss";
+
+import { ThemeProvider } from "@/components/theme-provider"
+
+import "./globals.css";
+import "@/styles/tokens/margins.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,15 +52,18 @@ export const metadata: Metadata = {
   authors: [{ name: "Yuntayki" }],
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
